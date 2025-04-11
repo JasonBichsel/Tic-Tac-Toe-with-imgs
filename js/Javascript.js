398a39697bfbb5e903ced3aca739d286db8edc18
JasonBichsel <jason.bichsel.b@gmail.com>
2025-04-11 11:23:31 +0200

    Add images and assign images to the variables

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,3 +1,8 @@
 const board = document.getElementById('board');
 const winnerDiv = document.getElementById('winner');
 const boardSizeSelector = document.getElementById('boardSize');
+
+const images = {
+    Red: 'img/Red.jpg', // Bild für Spieler Red
+    Blue: 'img/Blue.jpg' // Bild für Spieler Blue
+};
